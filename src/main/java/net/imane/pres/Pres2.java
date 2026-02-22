5963ed283c3597b7737e6d10a02bb5cf19966570
najib43imane <najibimane093@gmail.com>
2026-02-22 23:09:28 +0000

    commit -m "Faire un couplage faible de la couche presentation en utilisant le fichier config.txt"

---
+++ src/main/java/net/imane/pres/Pres2.java
@@ -0,0 +1,26 @@
+package net.imane.pres;
+
+import net.imane.dao.IDao;
+import net.imane.metier.IMetier;
+import net.imane.net.imane.ext.DaoImplV2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
+
+public class Pres2 {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Scanner scanner = new Scanner(new File("config.txt"));
+        String daoClassName = scanner.nextLine();
+        Class cDao = Class.forName(daoClassName);
+        IDao dao = (IDao) cDao.newInstance();
+
+        String metierClassName = scanner.nextLine();
+        Class cMetier = Class.forName(metierClassName);
+        IMetier metier = (IMetier) cMetier.getConstructor(IDao.class).newInstance(dao);
+
+        System.out.println("Resultat : "+metier.calcul());
+
+    }
+}
