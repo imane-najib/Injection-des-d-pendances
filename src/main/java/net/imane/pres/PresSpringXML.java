e13926cae4443d4c671f2f1e53a59d36cd960681
najib43imane <najibimane093@gmail.com>
2026-02-23 00:30:55 +0000

    commit -m "Injection des dépendances en utilisant le framework Spring version XML"

---
+++ src/main/java/net/imane/pres/PresSpringXML.java
@@ -0,0 +1,16 @@
+
+package net.imane.pres;
+
+import net.imane.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresSpringXML {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        IMetier metier = (IMetier) context.getBean("metier");
+
+        System.out.println("resultat : "+metier.calcul());
+    }
+}
